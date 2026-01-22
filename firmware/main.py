558f72b7eff7edf0404eebac732d4b5affe51471
atharvach2007 <128691862+atharvach2007@users.noreply.github.com>
2026-01-23 00:49:35 +0530

    Updated main.py

--- firmware/main.py
+++ firmware/main.py
@@ -7,9 +7,9 @@ from kmk.modules.oled import Oled, OledMode
 
 keyboard = KMKKeyboard()
 
-# -----------------------
-# Matrix
-# -----------------------
+# -------------------------------------------------
+# Matrix (4 rows x 3 columns)
+# -------------------------------------------------
 keyboard.row_pins = (
     board.GP1,   # Row 1
     board.GP2,   # Row 2
@@ -25,49 +25,57 @@ keyboard.col_pins = (
 
 keyboard.diode_orientation = keyboard.DIODE_COL2ROW
 
-# -----------------------
-# Keymap
-# -----------------------
-keyboard.keymap = [
-    [
-        KC.LCTRL,              KC.MEDIA_PLAY_PAUSE, KC.LALT,        # Row 1
-        KC.MEDIA_REWIND,       KC.F11,               KC.MEDIA_FAST_FORWARD,  # Row 2
-        KC.AUDIO_MUTE,         KC.UP,                KC.MEDIA_SUBTITLE,       # Row 3
-        KC.LEFT,               KC.DOWN,              KC.RIGHT,               # Row 4
-    ]
-]
+# -------------------------------------------------
+# Keymap (row-major order)
+# -------------------------------------------------
+keyboard.keymap = [[
+    KC.LCTRL,              KC.MEDIA_PLAY_PAUSE, KC.LALT,        # Row 1
+    KC.MEDIA_REWIND,       KC.F11,               KC.MEDIA_FAST_FORWARD,  # Row 2
+    KC.AUDIO_MUTE,         KC.UP,                KC.MEDIA_SUBTITLE,       # Row 3
+    KC.LEFT,               KC.DOWN,              KC.RIGHT,               # Row 4
+]]
 
-# -----------------------
-# Encoders
-# -----------------------
+# -------------------------------------------------
+# Encoder gating keys (matrix indices)
+# -------------------------------------------------
+ENC_BRIGHTNESS_KEY = 0   # (Row 1, Col 1)
+ENC_VOLUME_KEY     = 2   # (Row 1, Col 3)
+
+# -------------------------------------------------
+# Encoder setup (A/B shared)
+# -------------------------------------------------
 encoder = EncoderHandler()
-
 encoder.pins = (
-    (board.GP26, board.GP0),  # Encoder A & B
+    (board.GP26, board.GP0),
 )
 
+def enc_cw(keyboard):
+    if keyboard.key_states[ENC_BRIGHTNESS_KEY]:
+        keyboard.tap_key(KC.BRIGHTNESS_UP)
+    elif keyboard.key_states[ENC_VOLUME_KEY]:
+        keyboard.tap_key(KC.VOLU)
+
+def enc_ccw(keyboard):
+    if keyboard.key_states[ENC_BRIGHTNESS_KEY]:
+        keyboard.tap_key(KC.BRIGHTNESS_DOWN)
+    elif keyboard.key_states[ENC_VOLUME_KEY]:
+        keyboard.tap_key(KC.VOLD)
+
 encoder.map = [
-    (
-        KC.BRIGHTNESS_UP,     # Encoder A CW
-        KC.BRIGHTNESS_DOWN,   # Encoder A CCW
-    ),
-    (
-        KC.VOLU,              # Encoder B CW
-        KC.VOLD,              # Encoder B CCW
-    ),
+    (enc_cw, enc_ccw),
 ]
 
 keyboard.modules.append(encoder)
 
-# -----------------------
-# OLED
-# -----------------------
+# -------------------------------------------------
+# OLED (vertical)
+# -------------------------------------------------
 oled = Oled(
     sda=board.GP6,
     scl=board.GP7,
     width=128,
     height=32,
-    rotation=270,     # vertical
+    rotation=270,
     mode=OledMode.TEXT
 )
 
@@ -79,5 +87,8 @@ def oled_task(oled, keyboard):
 
 oled.task = oled_task
 
-if __name__ == '__main__':
+# -------------------------------------------------
+# Start
+# -------------------------------------------------
+if __name__ == "__main__":
     keyboard.go()
